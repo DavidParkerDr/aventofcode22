e1d2d3367da59515500e71675b259f16cfa52b1a
DavidParkerDr <david@goparker.com>
2022-12-01 12:00:19 +0000

    solved part 1

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                StreamReader sr = new StreamReader(args[0]);
+                Console.SetIn(sr);
+            }
+            bool complete = false;
+            List<int> allElvesCalories = new List<int>();
+            int currentElfCalories = 0;
+            while (!complete)
+            {
+                string input = Console.ReadLine();
+                if(input == null)
+                {
+                    complete = true;
+                }
+                if (string.IsNullOrEmpty(input))
+                {
+                    
+                    allElvesCalories.Add(currentElfCalories);  
+                    currentElfCalories = 0;
+                    continue;
+                }
+                int itemCalories = int.Parse(input);
+                currentElfCalories += itemCalories;
+            }
+            int highestElfCalories = 0;
+            foreach(int calories in allElvesCalories)
+            {
+                if (calories > highestElfCalories)
+                {
+                    highestElfCalories = calories;
+                }
+            }
+            Console.WriteLine(highestElfCalories);
+        }
+    }
+}
