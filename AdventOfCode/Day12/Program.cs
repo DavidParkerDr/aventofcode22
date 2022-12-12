39660a9227aea793a9a064a8a93ea42b19e1663b
DavidParkerDr <david@goparker.com>
2022-12-12 17:06:00 +0000

    Setup projects for Day 12 to Day 25 (just need to paste the input text in to the file.)

---
+++ AdventOfCode/Day12/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                StreamReader sr = new StreamReader(args[0]);
+                Console.SetIn(sr);
+            }
+            bool complete = false;
+            while (!complete)
+            {
+                string input = Console.ReadLine();
+                if (input == null)
+                {
+                    complete = true;
+                    continue;
+                }
+                // code here
+            }
+        }
+    }
+}
