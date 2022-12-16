3e1bd2e07641c232dd2f07971b906050cc446a34
DavidParkerDr <david@goparker.com>
2022-12-16 20:57:37 +0000

    Read in the input day 16

--- AdventOfCode/Day16/Program.cs
+++ AdventOfCode/Day16/Program.cs
@@ -10,6 +10,7 @@
                 Console.SetIn(sr);
             }
             bool complete = false;
+            Dictionary<string, Valve> valves = new Dictionary<string, Valve>();
             while (!complete)
             {
                 string input = Console.ReadLine();
@@ -18,8 +19,61 @@
                     complete = true;
                     continue;
                 }
-                // code here
+
+                input = input.Replace("Valve ", "");
+                string valveName = input.Substring(0, 2);
+                string[] parts = input.Split('=');
+                string secondPart = parts[1];
+                string[] afterTheEqualsParts = secondPart.Split(';');
+                string rateString = afterTheEqualsParts[0];
+                int rate = int.Parse(rateString);
+                string afterTheSemiColon = afterTheEqualsParts[1];
+                afterTheSemiColon = afterTheSemiColon.Replace(" tunnels lead to valves ", "");
+                afterTheSemiColon = afterTheSemiColon.Replace(" tunnel leads to valve ", "");
+                string[] valveParts = afterTheSemiColon.Split(", ");
+                Valve valve = new Valve(valveName, rate);
+                valves.Add(valve.Name, valve);
+                foreach(string valvePart in valveParts)
+                {
+                    valve.AddOtherValveString(valvePart);
+                }    
             }
+            foreach(KeyValuePair<string, Valve> pair in valves)
+            {
+                Valve valve = pair.Value;
+                foreach(string otherValveString in valve.OtherValveStrings)
+                {
+                    Valve otherValve = valves[otherValveString];
+                    valve.AddOtherValve(otherValve);
+                }
+            }
+            Valve startValve = valves["AA"];
+        }
+    }
+    class Valve
+    {
+        public string Name { get; set; }
+        public int Rate { get; set; }
+        public List<string> OtherValveStrings { get; set; }
+        public List<Valve> Valves { get; set; }
+        public Valve(string name, int rate)
+        {
+            Name = name;
+            Rate = rate;
+            OtherValveStrings = new List<string>();
+            Valves = new List<Valve>();
+        }
+        public void AddOtherValveString(string otherValveString)
+        {
+            OtherValveStrings.Add(otherValveString);
+        }
+        public void AddOtherValve(Valve otherValve)
+        {
+            Valves.Add(otherValve);
+        }
+        public override string ToString()
+        {
+            return Name + " rate: " + Rate;
         }
     }
 }
